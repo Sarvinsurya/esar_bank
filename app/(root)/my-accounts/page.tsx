4acd04b46a84ef69729dbf2330700f69811a90ca
Sarvinsurya K <sarvinsurya2704@gmail.com>
2024-10-02 17:15:10 +0530

    Implement the complete layout

---
+++ app/(root)/my-accounts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyAccounts = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default MyAccounts
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>TransactionHistoy</div>
+  )
+}
+
+export default TransactionHistory
